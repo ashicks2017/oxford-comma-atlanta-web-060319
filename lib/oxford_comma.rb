cbc5e8609d0a429116e22788e26ee5f700fbcee5
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 16:55:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,5 @@ if array.length == 1
   array.join(" , ")
 elsif array.length == 2 
   array.join("and")
-elsif array.length >= 3
- array[-1] = "and #{array[-1]}"
-return array.join("and")
- end
+end
 end
