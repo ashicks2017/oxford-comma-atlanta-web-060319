be4cbea33b3d226802a52d4d94add5a01450a20f
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 16:59:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@
 end
 def oxford_comma(array)
   if array.length == 1
-    return "#{array}"
+    return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
