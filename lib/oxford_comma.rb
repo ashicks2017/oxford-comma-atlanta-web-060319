171aa7a768cc27fccb257cbbd3399ca0eca3642b
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 16:58:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-if
+if array.length == 1 
+  return array
+  
   
   
   
@@ -7,7 +9,7 @@ if
 end
 def oxford_comma(array)
   if array.length == 1
-    return "#{array[0]}"
+    return "#{array}"
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
