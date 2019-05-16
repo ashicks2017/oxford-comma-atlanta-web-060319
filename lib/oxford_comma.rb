27bc3ad591a903b0b0a81015e49cc442fdcf5487
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 17:06:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,10 @@
 #type your code here
 def oxford_comma(array)
   if.array.length == 1 
-    return array.join
+    array.join
   elsif array.length == 2 
-   return array.join("and") 
+   array.join("and") 
  else array.length >=3
-   array[-1] = array.join("and")
+   array[-1] = "and #{array[-1]}"
+   array.join(",")
 end
-end
-#def oxford_comma(array)
-#  if array.length == 1
-#return "#{array[0]}"
-#  elsif array.length == 2
-#    return array.join(" and ")
-#  elsif array.length >= 3
-#    array[-1] = "and #{array[-1]}"
-#    return array.join(", ")
-#  end
-#end
