fccfb30571c155573b098f10df5ddbebb7c29c94
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 17:08:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
    return array.join
   elsif array.length == 2 
    return array.join("and") 
- else array.length >= 3
+ else
    array[-1] = "and #{array[-1]}"
    return array.join(",")
 end
