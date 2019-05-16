696354b8ccc8dd6635bbf5b272d703054375c6dd
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 16:56:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
+
+
 def oxford_comma(array)
-if array.length == 1 
-  array.join(" , ")
-elsif array.length == 2 
-  array.join("and")
-end
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
