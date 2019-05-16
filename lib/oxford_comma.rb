22117148bec534f83b20d338efb59280e06d8148
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 17:04:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,19 @@
 #type your code here
 def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
-  end
+  if.array.length == 1 
+    return array.join
+  elsif array.length == 2 
+   return array.join("and") 
+ else array.length >=3
+   array[-1] = array.join("and")
+end
+#def oxford_comma(array)
+#  if array.length == 1
+#return "#{array[0]}"
+#  elsif array.length == 2
+#    return array.join(" and ")
+#  elsif array.length >= 3
+#    array[-1] = "and #{array[-1]}"
+#    return array.join(", ")
+#  end
 end
